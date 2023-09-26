d307966423b259c388515a5ff7207c4c3ff8bd39
Bertonni <bertonnipaz@gmail.com>
2023-09-26 15:05:47 -0300

    including new routes

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ import { protectedRoute } from "./src/routes/protected.js";
 import { authRoute } from "./src/routes/auth.js";
 import { userDataRoute } from "./src/routes/user.js";
 import { categoriesRoute } from "./src/routes/categories.js";
+import { expensesRoute } from "./src/routes/expenses.js";
+import { incomesRoute } from "./src/routes/incomes.js";
 
 dotenv.config();
 
@@ -21,10 +23,11 @@ app.register(fastifyCors, {
 });
 
 app.register(authRoute);
-
 app.register(protectedRoute);
 app.register(userDataRoute);
 app.register(categoriesRoute);
+app.register(expensesRoute);
+app.register(incomesRoute);
 
 app.listen(
   {
---
+++ src/routes/incomes.js
@@ -0,0 +1,45 @@
+import { prisma } from '../lib/prisma.js';
+import { verifyJWT } from '../lib/utils.js';
+
+export async function incomesRoute(app) {
+  app.post("/incomes", { preHandler: verifyJWT }, async (req, reply) => {
+    try {
+      const { title, amount, reference, userId } = req.body;
+      console.log('body ===============>', req.body);
+      
+      await prisma.incomes.create({
+        data: {
+          title,
+          amount,
+          reference,
+          user: {
+            connect: {
+              id: userId
+            }
+          }
+        }
+      });
+
+      return reply.status(200).send({ message: 'Receita incluída com sucesso' });
+    } catch (error) {
+      return reply.status(400).send({ message: 'Nâo foi possível incluir a receita.' })
+    }
+  });
+
+  app.get("/incomes/:userId", { preHandler: verifyJWT }, async (req, reply) => {
+    
+    try {
+      const userId = req.params.userId;
+      const incomes = await prisma.incomes.findMany({
+        where: {
+          userId: userId,
+        }
+      });
+
+      return reply.status(200).send({ incomes });
+    } catch (error) {
+      return reply.status(200).send({ message: 'Não foi possível buscar as receitas' });
+    }
+  })
+}
+ 
